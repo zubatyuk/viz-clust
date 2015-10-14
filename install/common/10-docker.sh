bc21c906beb68a87e27d1d76393dd0c145f437e9
zubatyuk <zubatyuk@gmail.com>
2015-10-13 20:01:51 -0500

    Update 10-docker.sh

--- install/common/10-docker.sh
+++ install/common/10-docker.sh
@@ -3,7 +3,7 @@
 #install docker
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-apt-get update && apt-get install docker-engine curl
+apt-get update && apt-get install -y docker-engine curl
 
 #add group for admin user
 usermod -a -G docker admin 
