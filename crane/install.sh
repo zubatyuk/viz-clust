3dee028169206d4467deb30e9f00c97ca37db776
root <root@localhost.localdomain>
2015-10-13 20:45:36 -0500

    updates

--- crane/install.sh
+++ crane/install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 SCRIPTDIR=$(dirname $(readlink -f ${0%/*}))
-NAMES="apt-cacher-ng dnsmasq"
 
 function inst {
     DIR=$1
     NAME=$2
-    install ${DIR}/docker-${NAME}.conf /etc/init
+    install ${DIR}/upstart/docker-${NAME}.conf /etc/init
     sed -i -e "s|%INSTALLDIR%|$SCRIPTDIR|g" /etc/init/docker-${NAME}.conf 
-    service docker-${NAME} start
+    service docker-${NAME} restart
 }
 
 #installation
-inst apt-cacher-ng
-inst dnsmasq
+inst $SCRIPTDIR apt-cacher-ng
+inst $SCRIPTDIR dnsmasq
