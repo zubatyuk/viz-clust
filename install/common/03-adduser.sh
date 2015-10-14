cdc0ced05b96e2a92bd1b8520a18508ed8e3adeb
zubatyuk <zubatyuk@gmail.com>
2015-10-14 00:10:47 -0500

    Update 03-adduser.sh

--- install/common/03-adduser.sh
+++ install/common/03-adduser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-useradd -m -U admin
+useradd -m -U -s /bin/bash admin
 mkdir -p /home/admin/.ssh
 cat >> /home/admin/.ssh/authorized_keys << EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVIcYnwPY552HdRFMvk/eCFnjbWPuWtHaLgC1OpEY7Gsvgx/A7pHGXN35kHwrBRbjzU37roaf+3S4aP+5H/sgJ1cLPZyoHSk9ZsH2vfKqsKPmsMX++AltoFRcCU1qIAId8FMAw5DHVKMPh0zANqN9Z8d1x/ek3DFef1HvFd3T/WfzewfsiTbFeLKTIl0sdA4O23Pyvz8xcxXUcAtcms0NvrmcZ0L4pJQQ5IRJO1f76myAXom1yPWwf36KNcfTAlW/nFU3EmVmb2hgmKr/fkaG1L1S5vw2BwohchpHA6x0d+2kJRZ40qcFMAjfy2wneMpneC30SWX0d2uHaJCUS/fy5 admin@viz
