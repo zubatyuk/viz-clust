5f2c783fe22bd1da3a80497b8383e71393f4bc7b
zubatyuk <zubatyuk@gmail.com>
2015-10-14 00:23:16 -0500

    Update 41-kernel.sh

--- install/common/41-kernel.sh
+++ install/common/41-kernel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y linux-image-3.19.0-25-generic linux-headers-3.19.0-25-generic
+apt-get install -y linux-image-3.19.0-25-generic linux-headers-3.19.0-25-generic linux-image-extra-3.19.0-25-generic
